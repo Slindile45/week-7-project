341fb9d210b1a24dfbf2d735b763eb86d8a192c3
Slindile45 <168456312+Slindile45@users.noreply.github.com>
2024-06-10 11:42:58 +0200

    added search functionality using api

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+function searchCity(city) {
+    let apiKey = "33949tfa196efed4510bo9b38e1b5809";
+    let apiUrl = 
+
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
 
-    let searchIput = document.querySelector("#search-form-input");
+    let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchIput.value;
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+
 
 }
 
